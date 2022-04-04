dbc3d6357e868949661847564daed3d4b37e47a4
10Alexey01 <lex.mix.2002@yandex.ru>
2022-04-04 09:59:10 +0300

    4 задание

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using _3_TASK.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using LoginCheckLibrary;
+
+namespace _3_TASK
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Message = CheckClass.Check(LoginTextBox.Text, PasswordTextBox.Password);
+            if (Message == "Пользователь авторизован")
+            {
+                MessageBox.Show(Message);
+                RootWindow WT = new RootWindow();
+                WT.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(Message);
+            }
+        }
+    }
+}
---
+++ Pages/Client_List.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_TASK.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Client_List.xaml
+    /// </summary>
+    public partial class Client_List : Page
+    {
+        public Client_List()
+        {
+            InitializeComponent();
+        }
+
+        private void Reg_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string Name = nameTextBox.Text.Trim();
+            string LastName = lastNameTextBox.Text.Trim();
+            string MiddleName = middleNameTextBox.Text.Trim();
+            string PhoneNumber = phoneNumberTextBox.Text.Trim();
+        }
+    }
+}
